185b7d4cd21226e43de72e666c35a66fe4e7bf43
rizaievdima <rizaievdima@gmail.com>
2025-02-19 13:26:18 +0200

    template strings

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+let name = prompt("What is your name?");
+let surname = prompt("What is your surname?");
+let age = prompt("How old are you?");
+
+let text = `Your name is ${name} ${surname} and you are ${age} years old`;
+
+alert(text);
