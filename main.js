df789af41370664bf869add682827580e68ef71b
rizaievdima <rizaievdima@gmail.com>
2025-02-19 10:21:23 +0200

    main types

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+let myNumber = 42;
+let bigNumber = 900719925474091n;
+let myString = "hello";
+let boolValue = true;
+let noValue;
+let mySymbol = Symbol("logo");
+let myObj = {
+    a: 1,
+    b: 2
+};
+
+console.log(typeof (myNumber));
+console.log(typeof (bigNumber));
+console.log(typeof (myString));
+console.log(typeof (boolValue));
+console.log(typeof (noValue));
+console.log(typeof (mySymbol));
+console.log(typeof (myObj));
