a1d95a703ff6b001cfd036e3ad86e4586c81db12
rizaievdima <rizaievdima@gmail.com>
2025-02-24 13:18:47 +0200

    check numbers

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+let numberFromUser = +prompt("Write a number");
+let arrayNumbers = numberFromUser
+    .toString()
+    .split("")
+    .map((el) => +el);
+
+if (
+    arrayNumbers[0] === arrayNumbers[1] &&
+    arrayNumbers[1] === arrayNumbers[2]
+) {
+    console.log("All numbers the same");
+} else if (
+    arrayNumbers[0] === arrayNumbers[1] ||
+    arrayNumbers[0] === arrayNumbers[2] ||
+    arrayNumbers[1] === arrayNumbers[2]
+) {
+    console.log("Some numbers the same");
+} else {
+    console.log("All numbers different");
+}
