0eed8da7edec462ca3cc0ff57c358da0f4dd8234
rizaievdima <rizaievdima@gmail.com>
2025-02-27 19:51:59 +0200

    simple numbers

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let number = parseInt(prompt("write a number"));
+let numberType = "the number is simple";
+
+if (isNaN(number) || number <= 1) {
+    numberType = "the number is not simple";
+} else {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            numberType = "the number is not simple";
+            break;
+        }
+    }
+}
+
+console.log(numberType);
