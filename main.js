430dbb86fc6812cfb357380c6f3872bd6f9d741d
rizaievdima <rizaievdima@gmail.com>
2025-03-03 18:18:47 +0200

    remove item

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function removeElement(array, item) {
+    let i = 0;
+    while (i < array.length) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+}
+
+const array = [1, 3, 4, 6, 2, 4, 5, 7];
+
+removeElement(array, 4);
+
+console.log(array);
