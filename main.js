d69042c75550870c92d51b830d8876a78e82085d
rizaievdima <rizaievdima@gmail.com>
2025-03-03 16:37:13 +0200

    delete characters from array

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function removeCharacters(str, arrayToDelete) {
+    let result = str;
+    arrayToDelete.forEach((item) => {
+        result = result.replaceAll(item, "");
+    });
+    return result;
+}
+
+let newStr = removeCharacters("Hello World! How Are you?", ["e", "l", "?"]);
+console.log(newStr);
