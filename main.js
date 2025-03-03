9d7b47f760618b0253c1ecb9ea370f4c866742f5
rizaievdima <rizaievdima@gmail.com>
2025-03-03 17:27:46 +0200

    average number

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getAverageNumber(arr) {
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number" && !isNaN(arr[i])) {
+            sum += arr[i];
+            count++;
+        }
+    }
+    return count > 0 ? sum / count : 0;
+}
+
+let averageNumber = getAverageNumber([3, "3", "test", true, 4, null, 5, NaN]);
+console.log(averageNumber);
