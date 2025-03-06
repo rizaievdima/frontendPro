526400398c58f178cdcd5788727e97ce043b2a41
rizaievdima <rizaievdima@gmail.com>
2025-03-06 12:25:19 +0200

    number more then 100

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const maxRepeat = 10;
+
+for (let i = 0; i < maxRepeat; i++) {
+    let answer = +prompt("Ведіть число більше 100");
+
+    if (!isNaN(answer) && answer > 100) {
+        console.log(answer);
+        break;
+    }
+}
