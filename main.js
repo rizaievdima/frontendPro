360cd057ef9e7537948467c02b73674d6981a61f
rizaievdima <rizaievdima@gmail.com>
2025-03-06 11:57:54 +0200

    make sum function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function makeSum() {
+    let sum = 0;
+    return function (num) {
+        return (sum += num);
+    };
+}
+
+let sum = makeSum();
+
+console.log(sum(2));
+console.log(sum(2));
+console.log(sum(4));
