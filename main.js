fe400a771768fb3f700a84654a15d199ff5fe0cc
rizaievdima <rizaievdima@gmail.com>
2025-03-06 14:56:46 +0200

    number more then 100 v2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const maxRepeat = 10;
 for (let i = 0; i < maxRepeat; i++) {
     let answer = +prompt("Ведіть число більше 100");
 
-    if (!isNaN(answer) && answer > 100) {
+    if (answer && !isNaN(answer) && answer > 100) {
         console.log(answer);
         break;
     }
