96a2268dd46263e3bc8b915a84fc1b38493aeba9
rizaievdima <rizaievdima@gmail.com>
2025-03-12 09:05:26 +0200

    sum salaries using recursion

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const company = {
+    sales: [
+        { name: "John", salary: 1000 },
+        { name: "Alice", salary: 600 },
+    ],
+    development: {
+        web: [
+            { name: "Peter", salary: 2000 },
+            { name: "Alex", salary: 1800 },
+        ],
+        internals: [{ name: "Jack", salary: 1300 }],
+    },
+};
+
+function sumSalaries(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((acc, employee) => acc + employee.salary, 0);
+    }
+    return Object.values(department).reduce(
+        (acc, subDep) => acc + sumSalaries(subDep),
+        0
+    );
+}
+
+console.log(sumSalaries(company));
