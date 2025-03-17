b005b81c9e7c66842d8e19ac2ea4ac9828debe87
rizaievdima <rizaievdima@gmail.com>
2025-03-17 08:18:00 +0200

    even numbers

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbersArray.filter((number) => number % 2 === 0);
+
+console.log(evenNumbers);
