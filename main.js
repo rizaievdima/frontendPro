b22395b718063faef4b7a57ea2596087582f1328
rizaievdima <rizaievdima@gmail.com>
2025-03-17 08:20:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenNumbers = numbersArray.filter((number) => number % 2 === 0);
+const evenNumbers = numbersArray.filter(number => number % 2 === 0);
 
 console.log(evenNumbers);
