78c19885ad7568b2b1056a9c59cf5ab76f220615
rizaievdima <rizaievdima@gmail.com>
2025-03-17 08:07:34 +0200

    create usersObject

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const usersObject = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    showUserData() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, City: ${this.city}`);
+    },
+};
+
+usersObject.showUserData();
