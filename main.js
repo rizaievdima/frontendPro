81c63dbd48ba9c7ff56a67574d11574d2a1706cd
rizaievdima <rizaievdima@gmail.com>
2025-03-19 16:27:39 +0200

    random image

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const image = document.querySelector("#random-image");
+const button = document.querySelector("#change-image");
 
+const changeImage = () => {
+    let min = 1;
+    let max = 9;
+    let rundomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    image.src = `/images/${rundomNumber}.jpg`;
+};
+changeImage();
+
+button.addEventListener("click", changeImage);
