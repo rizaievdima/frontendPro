480074b5321f6d8b524298d344224a4c3133cc43
rizaievdima <rizaievdima@gmail.com>
2025-03-19 11:51:21 +0200

    toggle text color

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const button = document.querySelector("#my-button");
+const text = document.querySelector(".my-text");
 
+button.addEventListener("click", () => {
+    text.classList.toggle("color-green");
+});
