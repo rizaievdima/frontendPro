eb973dbd9ca1486a3fc9d609386567ff058bee2b
rizaievdima <rizaievdima@gmail.com>
2025-03-24 17:13:41 +0200

    3 buttons

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const buttonsContainer = document.querySelector(".buttons-container");
 
+buttonsContainer.addEventListener("click", (e) => {
+    if (e.target.id === "btn1") {
+        console.log("Button 1 clicked");
+    } else if (e.target.id === "btn2") {
+        console.log("Button 2 clicked");
+    } else if (e.target.id === "btn3") {
+        console.log("Button 3 clicked");
+    }
+});
