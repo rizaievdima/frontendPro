10aa48b1c99eb4629d582c14f3fc6cc4b79ae3e6
rizaievdima <rizaievdima@gmail.com>
2025-03-24 16:38:24 +0200

    write a link

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const linksContainer = document.querySelector(".links");
+const writeLink = document.querySelector("#write-link");
+const goLink = document.querySelector("#go-link");
 
+let url = "";
+
+linksContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("write-link")) {
+        url = prompt("Enter the URL").trim();
+    }
+
+    if (event.target.classList.contains("go-link")) {
+        if (url) {
+            window.location.assign(url);
+        } else {
+            console.log("Write correct URL");
+        }
+    }
+});
