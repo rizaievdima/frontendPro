b09b99ac00207e129a44f3e79ea3ef9e1691183e
rizaievdima <rizaievdima@gmail.com>
2025-04-10 12:03:05 +0300

    class Coach

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+class Coach {
+    constructor(name, speciality, rating) {
+        this.name = name;
+        this.speciality = speciality;
+        this.rating = rating;
+    }
+    displayInfo() {
+        console.log(`Coach: ${this.name}, Specialization: ${this.speciality}, Rating: ${this.rating}`);
+    }
+}
+const coach1 = new Coach("John Doe", "Fitness", 4.7);
+const coach2 = new Coach("Alice Smith", "Yoga", 4.9);
 
+coach1.displayInfo();
+coach2.displayInfo();
