39b72e073f7ee318ce1440cff6cc7c96f73fcc89
rizaievdima <rizaievdima@gmail.com>
2025-04-10 12:12:30 +0300

    class BankAccount

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+class BankAccount {
+    #balance;
 
+    constructor(initialBalance) {
+        this.#balance = initialBalance;
+    }
+
+    get balance() {
+        return this.#balance;
+    }
+
+    deposit(amount) {
+        if (amount <= 0) {
+            return;
+        }
+        this.#balance += amount;
+    }
+
+    withdraw(amount) {
+        if (amount <= 0 || amount > this.#balance) {
+            return;
+        }
+        this.#balance -= amount;
+    }
+}
+
+const account1 = new BankAccount(1000);
+
+console.log(account1.balance);
+account1.deposit(500);
+console.log(account1.balance);
+account1.withdraw(200);
+console.log(account1.balance);
