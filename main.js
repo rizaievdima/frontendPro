30e1cab5d53a70aa8fa96063937d87f4bdd1d7a8
rizaievdima <rizaievdima@gmail.com>
2025-04-16 12:59:06 +0300

    custom timer

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+let startTimer = 75;
 
+function rerenderTimer() {
+    const output = document.getElementById("timer");
+    const minutes = Math.floor(startTimer / 60)
+        .toString()
+        .padStart(2, "0");
+    const seconds = (startTimer % 60).toString().padStart(2, "0");
+
+    output.textContent = minutes + ":" + seconds;
+}
+
+const timer = setInterval(() => {
+    startTimer--;
+    rerenderTimer();
+    if (startTimer <= 0) {
+        clearInterval(timer);
+    }
+}, 1000);
+
+rerenderTimer();
